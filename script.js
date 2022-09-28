647121c51a1eed721ea14cd55ded2acacc9c8f34
BuildTools <unconfigured@null.spigotmc.org>
2022-09-28 23:05:52 +0300

    Some changes -removed warnings -refactored js function

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ window.onload = function () {
     function isInViewport(el) {
         const rect = el.getBoundingClientRect();
         return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            rect.top >= 0
         );
     }
 
