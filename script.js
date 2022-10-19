28c5f73af191985f2554f38eec21bba879a4260f
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 23:57:28 +0300

    Done 6th task

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 window.onload = function () {
+    let nav = document.getElementById("navigation");
+    let arrow = document.getElementById("arrowUp");
+    let burger_check = document.getElementById("menu");
+    let mediaQuery = window.matchMedia('(min-width: 660px)');
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', "nav.html");
+    xhr.addEventListener('readystatechange', function () { // load the page asynchronously
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // if the file is correctly loaded
+            document.getElementsByTagName('header')[0].innerHTML = xhr.responseText;
+            nav = document.getElementById("navigation");
+            arrow = document.getElementById("arrowUp");
+            burger_check = document.getElementById("menu");
+        }
+    });
+    xhr.send(null);
     //Checks if main menu is in viewport
     function isInViewport(el) {
         const rect = el.getBoundingClientRect();
@@ -6,36 +21,7 @@ window.onload = function () {
             rect.top >= 0
         );
     }
-    const nav = document.getElementById("navigation");
-    const arrow = document.getElementById("arrowUp");
-    const burger_check = document.getElementById("menu");
-    const burger_icon = document.getElementById("burger_icon");
-    const mediaQuery = window.matchMedia('(min-width: 660px)')
-    
-    //Animate opening burger menu
-    function rotateOpen() {
-        burger_check.checked = true;
-        burger_icon.animate([
-            {transform: 'rotate(0)'},
-            {transform: 'rotate(90deg)'}], 200);
-        burger_icon.style.transform = "rotate(90deg)";
-    }
-    //Animate closing burger menu
-    function rotateClose() {
-        burger_check.checked = false;
-        burger_icon.animate([
-            {transform: 'rotate(90deg)'},
-            {transform: 'rotate(0deg)'}], 200);
-        burger_icon.style.transform = "rotate(0deg)";
-    }
-    //Animate burger icon rotation
-    burger_check.addEventListener('change', function () {
-        if (burger_check.checked) {
-            rotateOpen();
-        } else {
-            rotateClose();
-        }
-    });
+
     //Shows arrow to the top of the page
     document.addEventListener('scroll', function () {
         isInViewport(nav) ? arrow.style.display = "none" : arrow.style.display = "block";
@@ -43,14 +29,13 @@ window.onload = function () {
     //Hide burger menu if changed window size
     window.addEventListener('resize', function () {
         if (mediaQuery.matches)
-            rotateClose();
+            burger_check.checked = false;
     })
     //Hide burger menu if clicked out of it
     document.addEventListener('click', function (e) {
         if (burger_check.checked === true)
-            if (e.target !== burger_check && e.target !== burger_icon && e.target.parentNode.parentNode.id !== "burger" && e.target.id !== "burger") {
-                rotateClose();
-                burger_icon.style.transform = "rotate(0deg)";
+            if (!e.target.classList.contains("bg_item")) {
+                burger_check.checked = false;
             }
     });
 }
